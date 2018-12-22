90087bb3a0829d703b786962c08d2e4596ee8ad1
DevilTea <ch870814@gmail.com>
2018-12-22 15:35:15 +0800

    Update util.js

--- src/api/util.js
+++ src/api/util.js
@@ -2,50 +2,70 @@
 import crypto from 'crypto'
 import eccrypto from 'eccrypto'
 
-const sha256Hash = (text) => crypto.createHash('sha256').update(text).digest('hex')
-
-const aesEncrypt = (plaintext, key, iv) => {
-  let cipher = crypto.createCipheriv('aes-256-cbc', key, iv)
-  let encrypted = cipher.update(plaintext, 'utf8', 'hex') + cipher.final('hex')
-  return encrypted
+export class SHA256 {
+  static hash (text) {
+    return crypto.createHash('sha256').update(text).digest('hex')
+  }
 }
 
-const aesDecrypt = (ciphertext, key, iv) => {
-  let decipher = crypto.createDecipheriv('aes-256-cbc', key, iv)
-  let decrypted = decipher.update(ciphertext, 'hex', 'utf8') + decipher.final('utf8')
-  return decrypted
+export class AES {
+  static encrypt (plaintext, key, iv) {
+    console.log(plaintext, key, iv)
+    let cipher = crypto.createCipheriv('aes-256-cbc', key, iv)
+    let encrypted = cipher.update(plaintext, 'utf8', 'hex') + cipher.final('hex')
+    return encrypted
+  }
+  static decrypt (ciphertext, key, iv) {
+    let decipher = crypto.createDecipheriv('aes-256-cbc', key, iv)
+    let decrypted = decipher.update(ciphertext, 'hex', 'utf8') + decipher.final('utf8')
+    return decrypted
+  }
 }
 
-const ecEncrypt = async (key, plaintext) => {
-  let ciphertext = await eccrypto.encrypt(Buffer.from(key, 'hex'), Buffer.from(plaintext, 'utf8'))
-
-  Object.keys(ciphertext).forEach(key => {
-    ciphertext[key] = ciphertext[key].toString('hex')
-  })
-
-  return ciphertext
+export class EC {
+  static async encrypt (plaintext, publicKey) {
+    let ciphertext = await eccrypto.encrypt(Buffer.from(publicKey, 'utf8'), Buffer.from(plaintext, 'utf8'))
+    Object.keys(ciphertext).forEach(key => {
+      ciphertext[key] = ciphertext[key].toString('hex')
+    })
+    return ciphertext
+  }
+  static async decrypt (ciphertext, privateKey) {
+    Object.keys(ciphertext).forEach(key => {
+      ciphertext[key] = Buffer.from(ciphertext[key], 'hex')
+    })
+    let plaintext = await eccrypto.decrypt(Buffer.from(privateKey, 'hex'), ciphertext)
+    return plaintext.toString('utf8')
+  }
 }
 
-const ecDecrypt = async (key, ciphertext) => {
-  Object.keys(ciphertext).forEach(key => {
-    ciphertext[key] = Buffer.from(ciphertext[key], 'hex')
-  })
-
-  let plaintext = await eccrypto.decrypt(Buffer.from(key, 'hex'), ciphertext)
-
-  return plaintext.toString('utf8')
-}
-
-export const SHA256 = {
-  hash: sha256Hash
-}
-
-export const AES = {
-  encrypt: aesEncrypt,
-  decrypt: aesDecrypt
-}
-
-export const EC = {
-  encrypt: ecEncrypt,
-  decrypt: ecDecrypt
+export class MessageHandler {
+  static encrypt (senderUsername, message, key) {
+    let iv = SHA256.hash(senderUsername).slice(0, 16)
+    switch (message.type) {
+      case 'text':
+        console.log('yee', message)
+        MessageHandler.encryptTextMessage(message, key, iv)
+        console.log('yeee', message)
+        break
+      default:
+    }
+  }
+  static decrypt (message, key) {
+    let iv = SHA256.hash(message.sender).slice(0, 16)
+    switch (message.type) {
+      case 'text':
+        MessageHandler.decryptTextMessage(message, key, iv)
+        break
+      default:
+        message.type = 'text'
+        message.content = 'Unsupported type content'
+    }
+  }
+  static encryptTextMessage (message, key, iv) {
+    message.content = AES.encrypt(message.content, key, iv)
+  }
+  static decryptTextMessage (message, key, iv) {
+    message.content = AES.decrypt(message.content, key, iv)
+  }
 }
